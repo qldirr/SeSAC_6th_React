a48a59d7bcebe2408bfd34e90fe436fd545a5e0b
qldirr <sub05073@naver.com>
2024-08-07 18:53:09 +0900

    study: react map/filter 실습

---
+++ 5-1-map-filter-practice/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Email from './Email';
+
+function App() {
+  return (
+    <div className="App">
+      <Email/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 5-1-map-filter-practice/src/Email.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+export default function Email() {
+    const [info, setInfo] = useState([
+        {
+            id: 1,
+            name: '코디',
+            email: 'codi@gmail.com'
+        },
+        {
+            id: 2,
+            name: '김어진',
+            email: 'djwls0803@gmail.com'
+        },
+    ])
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    // 등록
+    const submitInfo = () => {
+        if(name.trim().length !== 0 && email.trim().length !== 0){
+            const newInfo = info.concat({
+                id:info.length+1,
+                name:name,
+                email:email
+            })
+
+            setInfo(newInfo)
+            setEmail('')
+            setName('')
+        }
+    }
+
+    // 삭제
+    const deleteInfo = (targetId) => {
+        const newInfo = info.filter((info) => info.id !== targetId)
+        setInfo(newInfo)
+    }
+
+    return (
+        <div>
+
+            <input type="text" placeholder='이름' value={name} onChange={(e) => setName(e.target.value)}/>
+            <input type="text" placeholder='이메일' value={email} onChange={(e) => setEmail(e.target.value)}  />
+            <button onClick={submitInfo}>등록</button>
+
+            <ol style={{listStyle:'none'}}>
+                {info.map((val) => (
+
+                    <li key={val.id} onDoubleClick={() => deleteInfo(val.id)}><b>{val.name} : {val.email}</b></li>
+                ))}
+            </ol>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
