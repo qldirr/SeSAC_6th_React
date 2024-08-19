a21a6ed8710d02cd89aecfeb638dc6ba25207f69
qldirr <sub05073@naver.com>
2024-08-19 17:11:41 +0900

    study: react redux

---
+++ 10-redux/src/App3.js
@@ -0,0 +1,80 @@
+import { useDispatch, useSelector } from 'react-redux';
+import './styles/Box.css'
+import { minus, plus } from './store/counterReducer';
+import { changeVisibility } from './store/isVisibleReducer';
+
+function App3() {
+    // 7. useSelector 훅 사용해 Redux 스토어에서 상태 읽어오기
+    // Redux 상태에서 number 값 선택
+    // const number = useSelector((state) =>state)
+    // const number = useSelector((state) =>state.number)
+    const number = useSelector((state) => state.counter.number)
+    // console.log('number,.,.,.,.,', number);   // {number : 100}
+    
+  
+  return (
+    <div className="App">
+      <h1>React Redux Ex3</h1>
+      <h2>Redux를 사용</h2>
+      <h2>리듀서</h2>
+      <h2>number : {number}</h2>
+      <Box1 />
+    </div>
+  );
+}
+
+
+
+// Box1 컴포넌트
+const Box1 = () => {
+  return (
+    <div className='Box'>
+      <h2>Box1 : </h2>
+      <Box2 />
+    </div>
+  )
+}
+
+// Box2 컴포넌트
+const Box2 = () => {
+  return (
+    <div className='Box2'>
+      <h2>Box2 : </h2>
+      <Box3 />
+    </div>
+  )
+}
+
+// Box3 컴포넌트
+const Box3 = () => {
+  return (
+    <div className='Box3'>
+      <h2>Box3 : </h2>
+      <Box4 />
+    </div>
+  )
+}
+
+// Box4 컴포넌트
+const Box4 = () => {
+    // 8. useDispatch 훅 사용해 액션을 디스패치하는 함수 가져오기
+    // - 이 함수 사용해 상태 업데이트
+    // Redux 상태에서 number 값 선택하고 액션을 디스패치할 준비
+    // const state = useSelector((state) => state.counter)
+    const number = useSelector((state) => state.counter.number)
+    const isVisible = useSelector((state) => state.isVisible)
+    const dispatch = useDispatch()
+  return (
+    <div className='Box4'>
+      <h2>Box4 : {number}</h2>
+      <h2>isVisible 값은 " {isVisible ? '참' : '거짓'} " 이다</h2>
+      <button onClick={() => dispatch(plus())}>PLUS</button>
+      <button onClick={() => dispatch(minus())}>MINUS</button>
+
+      <button onClick={() => dispatch(changeVisibility())}>CHANGE</button>
+    </div>
+  )
+}
+
+
+export default App3;
---
+++ 10-redux/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App2 from './App2';
+import App3 from './App3';
+import rootReducer from './store';
+import App4 from './App4';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 6. Store 생성
+// (전통 Redux 방식) - 'createStore'를 사용해 Redux 스토어 생성
+const store = createStore(rootReducer)
+root.render(
+  <React.StrictMode>
+    {/* React 와 Redux 연결 */}
+    {/* 애플리케이션의 모든 컴포넌트가 Redux 스토어에 접근 가능하게됨 */}
+    <Provider store={store}>   {/* Redux Provider로 스토어를 앱에 주입 */}
+      <App />
+      <hr />
+      <App2 />
+      <hr />
+      <App3/>
+      <hr />
+      <App4/>
+    </Provider>
+  </React.StrictMode>
+);
+
